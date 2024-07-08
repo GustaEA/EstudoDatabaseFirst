d365a334cb0816f6a80718c2801960b32e92438b
Usuario <Usuario@DESKTOP-N9THCF6>
2024-07-08 17:40:07 -0300

    Adicionar arquivos de projeto.

---
+++ EstudoDatabaseFirst/Controllers/CostumerController.cs
@@ -0,0 +1,33 @@
+﻿using EstudoDatabaseFirst.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstudoDatabaseFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CostumerController : ControllerBase
+    {
+        private readonly AdventureWorksLt2022Context _context;
+
+        public CostumerController(AdventureWorksLt2022Context context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Customer>>> GetCustomers()
+        {
+            List<Customer> customers = await _context.Customers.ToListAsync();
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+            return Ok(customer);
+        }
+    }
+}
